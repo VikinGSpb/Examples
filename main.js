d20d7340f6c5a7b2c347973967c8289a5374c81c
VikinGSpb <imbot@inbox.ru>
2019-08-16 23:29:42 +0300

    4 new methods for complex

--- main.js
+++ main.js
@@ -3,12 +3,42 @@ class Complex{
         this.real = real;
         this.imaginary = imaginary;
     }
+
     Sum(CNumber){
         let result = new Complex(0,0);
         result.real = this.real + CNumber.real;
         result.imaginary = this.imaginary + CNumber.imaginary;
         return result;
     }
+
+    Sub(CNumber){
+        let result = new Complex(0,0);
+        result.real = this.real - CNumber.real;
+        result.imaginary = this.imaginary - CNumber.imaginary;
+        return result;
+    }
+
+    MultComplex(CNumber){
+        let result = new Complex(0,0);
+        result.real = this.real * CNumber.real - this.imaginary * CNumber.imaginary;
+        result.imaginary = this.real * CNumber.imaginary + CNumber.real * this.imaginary;
+        return result;
+    }
+
+    MultNumber(RNumber){
+        let result = new Complex(0,0);
+        result.real = this.real * RNumber;
+        result.imaginary = this.imaginary * RNumber;
+        return result;
+    }
+
+    Conjugate(){
+        let result = new Complex(0,0);
+        result.real = this.real;
+        result.imaginary = -this.imaginary;
+        return result;
+    }
+
     toString(){
         return `${this.real}, ${this.imaginary}`;
     }
@@ -16,4 +46,8 @@ class Complex{
 
 let first = new Complex(1,1);
 let second = new Complex(2,3);
-alert(first.Sum(second));
+alert('first + second = ' + first.Sum(second));
+alert('first - second = ' + first.Sub(second));
+alert('first * second = ' + first.MultComplex(second));
+alert('(1,1) * 5 = ' + first.MultNumber(5));
+alert('conjugate ' + first.Conjugate());
